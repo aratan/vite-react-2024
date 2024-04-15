d34db5b5a6f001db37fe474d71c4b6fbae4664ed
victor <victor.arbiol@gmail.com>
2024-04-15 20:29:06 +0200

    update

--- src/About.jsx
+++ src/About.jsx
@@ -2,7 +2,7 @@ export function About() {
     return (
       <>
         <h1>Página Acerca de nosotros</h1> {/* Using Spanish for consistency */}
-        <a href="/">INICIO</a> {/* Using Spanish for consistency */}
+         {/* <a href="/">INICIO</a> Using Spanish for consistency */}
       </>
     );
   }
--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ export function Home() {
     return (
       <>
         <h1>Página de inicio - HOME</h1> {/* Using Spanish for consistency */}
-        <a href="/about">Sobre mí</a> {/* Using Spanish for consistency */}
+         {/* <a href="/about">Sobre mí</a> Using Spanish for consistency */}
       </>
     );
   }
